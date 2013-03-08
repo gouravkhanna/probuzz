43f06c4cce0349dae6584f34b78e0d71559a14d5
gouravkhanna <gourav.khanna@osscube.com>
2013-03-08 09:08:55 -0800

    corp.php changed

--- corp.php
+++ corp.php
@@ -5,9 +5,9 @@ $profile_pic_path="data/photo/g.jpg";
 
 ?>
 <?php
-  include_once 'class/lang/lang.en.php';
-  include_once 'class/constant.php';
-  
+	include_once 'class/lang/lang.en.php';
+	include_once 'class/constant.php';
+	
 ?>
 <!doctype html>
 <html>
@@ -25,71 +25,69 @@ $profile_pic_path="data/photo/g.jpg";
 <body>
 
 <div id="container">
-  <div id="head1" >
-      <h1> 
-        <?php echo PROBUZZ; ?>     
-      </h1>
-        <a href="index.php"><?php echo HOME; ?></a>
-        <a href="corp.php"><?php echo CHOME; ?></a>
-        <a href="index.php?url=logout"><?php echo LOGOUT; ?></a>
-        <progress value=10 max=100></progress>
-  </div>
-  <div id="mid1" >
+	<div id="head1" >
+			<h1> 
+				<?php echo PROBUZZ; ?>     
+			</h1>
+				<a href="index.php"><?php echo HOME; ?></a>
+				<a href="corp.php"><?php echo CHOME; ?></a>
+				<a href="index.php?url=logout"><?php echo LOGOUT; ?></a>
+				<progress value=10 max=100></progress>
+	</div>
+	<div id="mid1" >
 
-      <div id="photo">
-        <img class="photo" src="<?php echo $profile_pic_path;?>" height="80" width="80">
-        <span class="alignwelcome"> <?php echo WELCOME; ?> <br/>
-        <?php echo $_SESSION['user_name'];
+			<div id="photo">
+				<img class="photo" src="<?php echo $profile_pic_path;?>" height="80" width="80">
+				<span class="alignwelcome"> <?php echo WELCOME; ?> <br/>
+				<?php echo $_SESSION['user_name'];?>
+			</span> 
+			 </div>      
 
-        ?>oooooooo </span> 
-       </div>      
-
-        <a href="index.php?req=profile">Social Profile</a> 
-        <a href="index.php?req=pprofile">Professional Profile</a>
-        <a href="index.php?req=cprofile" >Cprofile</a> 
-        <a href="index.php?req=jobs">Create Job</a>
-      
-          <!-- INCLUDE FIle here (using include tag) -->
-       
-   </div>
-   
-    <div id="head2" >
-    
-    </div>
-   
-    <div id="mid" >
-    <div id="mid2" >
-         <!-- INCLUDE FIle here (using include tag) -->
-      <?php
-      if(@$_REQUEST['req']=="") include_once 'views/buzz.php';
-      if(@$_REQUEST['req']=="cprofile") include_once 'gdata/cprofile.php';
-      if(@$_REQUEST['req']=="jobs") include_once 'gdata/prejob.php';
-      if(@$_REQUEST['req']=="createJob" && isset($_REQUEST['jobId'])) include_once 'gdata/jobs.php';
-    
-    ?>
-    
-        </div>
+				<a href="index.php?req=profile">Social Profile</a> 
+				<a href="index.php?req=pprofile">Professional Profile</a>
+				<a href="index.php?req=cprofile" >Cprofile</a> 
+				<a href="index.php?req=jobs">Create Job</a>
+				<!-- INCLUDE FIle here (using include tag) -->
+			 
+		</div>
+	 
+		<div id="head2" >
+		
+		</div>
+	 
+		<div id="mid" >
+		<div id="mid2" >
+				 <!-- INCLUDE FIle here (using include tag) -->
+			<?php
+			if(@$_REQUEST['req']=="") include_once 'views/buzz.php';
+			if(@$_REQUEST['req']=="cprofile") include_once 'gdata/cprofile.php';
+			if(@$_REQUEST['req']=="jobs") include_once 'gdata/prejob.php';
+			if(@$_REQUEST['req']=="createJob" && isset($_REQUEST['jobId'])) include_once 'gdata/jobs.php';
+		
+		?>
+		
+				</div>
 
 
-    <div id="midm" >
-  
-          <div id="mid3" >
-    <h1>Notifications</h1>
-          </div>
-    <div id="mid35"  >
-              <h1>Top Jobs</h1>
-            <!-- INCLUDE FIle here (using include tag) -->
-     </div>
-         </div>
-    </div>
+		<div id="midm" >
+	
+					<div id="mid3" >
+		<h1>Notifications</h1>
+					</div>
+		<div id="mid35"  >
+							<h1>Top Jobs</h1>
+						<!-- INCLUDE FIle here (using include tag) -->
+		 </div>
+				 </div>
+		</div>
 
-    
-    
+		
+		
 <div id="footer" >
 <footer> Copyright ProBuzZ || All Right Reserved || 2013  </footer>
 
 </div>
-    
+		
 
 </div>
 
