9aaabfac4909f93e536ab9388d3f91fca0746cfe
gouravkhanna <gourav.khanna@osscube.com>
2013-03-13 11:37:34 -0700

    added alotslot.php

---
+++ views/rightpanel/alotslot.php
@@ -0,0 +1,13 @@
+<div id="rightpanel1">
+	<div id="createSlot">
+	<h1>CREATE A NEW Slot</h1>
+	<form method="" action="">
+	<?php echo DESIGNATION; ?>
+	<input type="text" name="designation"> 	<br/>
+	<?php echo ACCEPT_TERMS; ?> 
+	<input type="checkbox" name="terms" value="true"><br/>
+	<input type=submit name="url" value="alotSlot">
+<hr/>
+</form>
+</div>
+</div>
