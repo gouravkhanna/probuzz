cc2c1c559c8ef749493e8a408ff597e52b37b0de
gouravkhanna <gourav.khanna@osscube.com>
2013-04-02 04:57:23 -0700

    styling of displaysearh and display application

--- views/displayapplication.php
+++ views/displayapplication.php
@@ -1,22 +1,20 @@
 <div id="bigmid">
 <div id="sjsearchres">
 <?php if (! empty ( $arrData )) {
-	echo "<div id='sjresulttable' ><div id='sjresultrow'><div id='sjresultcol'>DESIGNATION</div><div id='sjresultcol'>LOCATION</div><div id='sjresultcol'>POSTED BY</div><div id='sjresultcol'>START DATE</div><div id='sjresultcol'>END DATE</div></div>";
 	foreach ( $arrData as $key => $val ) {
 		if ($val ['id'] != "") {
 			$res = " <div id='sjresultrow' class=floatl> ";
-			$res .="<button onclick=fnLoadJobUser2('" . $val ['id'] . "') >";
-			$res .= "<div id='sjresultcol' >".$val['designation']."</div> ";
-			$res .= "<div id='sjresultcol' >".$val['location']."</div> ";
+			$res .="<button id='sjbtn' onclick=fnLoadJobUser2('" . $val ['id'] . "') >";
 			$res .= "<div id='sjresultcol' >".$val['company_name']."</div> ";
-			$res .= "<div id='sjresultcol' >".$val['appliying_date']."</div> ";
-			$res .= "<div id='sjresultcol' >".$val['id']."</div> ";
+			$res .= "<div id='sjresultcol' >".$val['designation']."</div> <hr>";
+			$res .= "<div id='sjresultcol' >".$val['location']."</div> ";
+				$res .= "<div id='sjresultcol' >".$val['appliying_date']."</div> ";
 			$res .= "</button>";
 			$res .= "</div>";
 			echo $res;
 		}
 	}
-	echo "</div>";
+
 }
 ?>
 
--- views/search/displaysearchjob.php
+++ views/search/displaysearchjob.php
@@ -1,26 +1,35 @@
+<br/><br/><br/>
 <?php
-echo "<pre>";
+
 //	print_r($arrData);
 
 echo "<div id='sjresdata'>";
 if (! empty ( $arrData[0] )) {
-	echo "<div id='sjresulttable' ><div id='sjresultrow'><div id='sjresultcol'>DESIGNATION</div><div id='sjresultcol'>LOCATION</div><div id='sjresultcol'>POSTED BY</div><div id='sjresultcol'>START DATE</div><div id='sjresultcol'>END DATE</div></div>";
+	/* echo "<div id='sjresulttable' ><div id='sjresultrow'><div id='sjresultcol'>DESIGNATION</div>
+		<div id='sjresultcol'>LOCATION</div><div id='sjresultcol'>POSTED BY</div>
+		<div id='sjresultcol'>START DATE</div>
+		<div id='sjresultcol'>END DATE</div>
+		</div>"; */
 	foreach ( $arrData as $key => $val ) {
 		if ($val ['jobid'] != "") {
-			$res = " <div id='sjresultrow' class=floatl> ";
-			$res .="<button onclick=fnLoadJobSearch('" . $val ['jobid'] . "') >";
+			$res = " <div id='sjresultrow' class=floatl > ";
+			$res .="<button id='sjbtn' onclick=fnLoadJobSearch('" . $val ['jobid'] . "') >";
+			$res .= "<div id='sjresulthead' >".$val['company_name']."</div><hr/> ";
 			$res .= "<div id='sjresultcol' >".$val['designation']."</div> ";
 			$res .= "<div id='sjresultcol' >".$val['location']."</div> ";
-			$res .= "<div id='sjresultcol' >".$val['company_name']."</div> ";
-			$res .= "<div id='sjresultcol' >".$val['startdate']."</div> ";
-			$res .= "<div id='sjresultcol' >".$val['lastdate']."</div> ";
+			if(!empty($val['startdate'])) {
+				$res .= "<div id='sjresultcol' > Start Date ".$val['startdate']." ";
+			}
+			if(!empty($val['lastdate'])) {
+				$res .= "  End Date ".$val['lastdate']."</div> ";
+			}
 			$res .= "</button>";
 			$res .= "</div>";
 			
 			echo $res;
 		}
 	}
-	echo "</div>";
+	//echo "</div>";
 }
 
 echo "</div>"; 
