f68b7eec70fb86a6f5b97c46a52a5ea452c36184
bhanu007 <bhanu.gill@osscube.com>
2013-04-03 10:36:21 -0700

    buzz button displayed

--- class/personalprofile.class.php
+++ class/personalprofile.class.php
@@ -1,41 +1,86 @@
 <?php
 include_once 'class/dbAcess.php';
 
-class personalprofile {
+class personalprofile extends DbConnection {
 
 
 function __construct()
   {
-  
+    parent::__construct ();
   }
 function loadProfile($arrArgs=array()) {
  $ob=new DbConnection();
   $arrData=array("bhanu"=>"yello");
   $id=$arrArgs['id'];
   
-  $sql=" SELECT first_name,last_name,DOB,contact_no,gender,email,
+  /*$sql=" SELECT first_name,last_name,DOB,contact_no,gender,email,
         about_myself,relationship_status,intersted_in,
         favourite_book,favourite_movies,favourite_food 
         FROM personal_profile WHERE user_id='$id' ";
-  $res=$ob->executeSQL($sql);
-  $row=mysql_fetch_array($res);
+  */
+    $data ['tables'] = "personal_profile ";
+    $data ['columns'] = array (
+                    'first_name',
+                    'last_name',
+                    'DOB',
+                    'contact_no',
+                    'gender',
+                    'email',
+                    'about_myself',
+                    'relationship_status',
+                    'intersted_in',
+                    'favourite_book',
+                    'favourite_movies',
+                    'favourite_food' 
+            );
+   $data ['conditions'] = array (
+                    'user_id' => "$id" 
+            );
+  $res = $this->db->select ( $data );
+  //$res=$ob->executeSQL($sql);
+  $row=$res->fetch ( PDO::FETCH_ASSOC );
   
-  $sql1="SELECT company_Name,current_job,position  FROM 
-         experience WHERE user_id='$id'";
-  $res1=$ob->executeSQL($sql1);
-  $row1=mysql_fetch_array($res1);
+  /*$sql1="SELECT company_Name,current_job,position  FROM 
+         experience WHERE user_id='$id'";*/
+   $data ['tables'] = "experience";
+    $data ['columns'] = array ('company_Name',
+                               'current_job',
+                                'position'
+                                 );
+  $data ['conditions'] = array (
+                    'user_id' => "$id" 
+            );
+  $res1=$this->db->select ( $data );
+  $row1=$res1->fetch ( PDO::FETCH_ASSOC );
   
-  $sql2="SELECT house_number,street_number,street_name,
+/*  $sql2="SELECT house_number,street_number,street_name,
          city,state,district,country FROM address 
-          WHERE user_id='$id'";
-  $res2=$ob->executeSQL($sql2);
-  $row2=mysql_fetch_array($res2);
+          WHERE user_id='$id'";*/
+   $data ['tables'] = "address";
+   $data ['columns'] = array ('house_number',
+                               'street_name',
+                              'street_number','city','state', 
+                              'district','country'
+                              );
+    $data ['conditions'] = array (
+                    'user_id' => "$id" 
+            );
+  $res2=$this->db->select ( $data );
+  $row2=$res2->fetch ( PDO::FETCH_ASSOC );
   
-  $sql3="SELECT institute ,university from qualification
+  /*$sql3="SELECT institute ,university from qualification
           WHERE user_id='$id' ";
+*/
 
-  $res3=$ob->executeSQL($sql3);
-  $row3=mysql_fetch_array($res3);        
+           $data ['tables'] = "qualification";
+           $data ['columns'] = array ('institute',
+                                       'university'
+                                    );
+           $data ['conditions'] = array (
+                    'user_id' => "$id" 
+            );
+  $res3=$this->db->select ( $data );
+  $row3=$res3->fetch ( PDO::FETCH_ASSOC );       
   $arrData=array(
         'personal'=>$row, 
         'experience'=>$row1,
