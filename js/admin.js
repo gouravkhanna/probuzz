0f972b06d7268aed0ebdb00d71e9fccda9f28f57
gouravkhanna <gourav.khanna@osscube.com>
2013-04-07 16:34:50 -0700

    added new admin.js

---
+++ js/admin.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $("#buzztable").dataTable(); 
+    $("#showba11").hide();
+});
+
+function clearSpam(a) {
+    alert(a);
+    if(confirm("Sure You want to Delete,\n It Wont Come Back!")) {   
+        b="#row"+a;
+        $(b).slideUp("slow");
+        c="#usshowmsg";
+        $(c).load("index.php","controller=admin&url=updateReview&spam_id="+a+"&spam_type=0");
+      //  $(c).append("Buzz Deleted<br/> Please Find it in Resolved Page");
+    }
+}  
+function clearUserSpam(a) {
+    alert(a);
+    if(confirm("Sure You want to Delete,\n It Wont Come Back!")) {   
+        b="#row"+a;
+        $(b).slideUp("slow");
+        c="#usshowmsg";
+        $(c).load("index.php","controller=admin&url=updateReview&spam_id="+a+"&spam_type=1");
+      //  $(c).append("Buzz Deleted<br/> Please Find it in Resolved Page");
+    }
+}  
+function buzzDeleteAdmin(a) {
+    alert(a);
+    if(confirm("Sure You want to Delete,\n It Wont Come Back!")) {   
+        b="#row"+a;
+        $(b).slideUp("slow");
+        c="#usshowmsg";
+        $(c).load("index.php","controller=admin&url=buzzAdminDelete&spam_id="+a+"&spam_type=0");
+      //  $(c).append("Buzz Deleted<br/> Please Find it in Resolved Page");
+    }
+}
+function banUserOneDay(a) {
+    alert(a);
+    if(confirm("Sure You want to Delete,\n It Wont Come Back!")) {   
+        b="#row"+a;
+        $(b).slideUp("slow");
+        c="#usshowmsg";
+        $(c).load("index.php","controller=admin&url=banUserOneDay&spam_id="+a+"&spam_type=1");
+      //  $(c).append("Buzz Deleted<br/> Please Find it in Resolved Page");
+    }
+}
+function banUserOneDayBuzz(a) {
+    alert(a);
+    if(confirm("Sure You want to Delete,\n It Wont Come Back!")) {   
+        b="#row"+a;
+        $(b).slideUp("slow");
+        c="#usshowmsg";
+        $(c).load("index.php","controller=admin&url=banUserOneDay&spam_id="+a+"&spam_type=0");
+      //  $(c).append("Buzz Deleted<br/> Please Find it in Resolved Page");
+    }
+}
+function banUserOnePermanent(a) {
+    alert(a);
+    if(confirm("Sure You want to Delete,\n It Wont Come Back!")) {   
+        b="#row"+a;
+        $(b).slideUp("slow");
+        c="#usshowmsg";
+        $(c).load("index.php","controller=admin&url=banUserOnePermanent&spam_id="+a+"&spam_type=1");
+      //  $(c).append("Buzz Deleted<br/> Please Find it in Resolved Page");
+    }
+}
