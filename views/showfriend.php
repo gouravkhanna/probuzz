3e85f205fe7a8d1b74f17a350cba9d67cb6ab738
bhanu007 <bhanu.gill@osscube.com>
2013-04-10 08:47:41 -0700

    friends display

--- views/showfriend.php
+++ views/showfriend.php
@@ -4,7 +4,7 @@
 
 <?php
 
-echo "<div id=dvresbig  >";
+echo "<div id=bige >";
 
 if (! empty ( $arrData )) {
 		echo "<div id='showfrnddiv'><h2>Friends </h2></div> ";
@@ -12,8 +12,8 @@ if (! empty ( $arrData )) {
 		if ($val ['id'] != "") {
 			$res = "<a href='index.php?controller=friends&url=friendsProfile&friendId=" . $val ['id'] . "' >";
 			$res .= "<div id=dvres> ";
-			$res .= "<img class='imgcenter floatl round5' src='" . $val ['path'] . "' height='75' width='75' >";
-			$res .= "<aside >" . $val ['first_name'] . "</aside><aside> " . $val ['last_name'] . "<aside></div>";
+			$res .= "<img class='imgcenterm floatl round5' src='" . $val ['path'] . "' height='75' width='75' >";
+			$res .= "<aside class='floatr fontsize16'>" . $val ['first_name'] . "</aside><br><br><aside class ='floatr fontsize14'> " . $val ['last_name'] . "<aside></div>";
 			$res .= "</a>";
 			echo $res;
 
