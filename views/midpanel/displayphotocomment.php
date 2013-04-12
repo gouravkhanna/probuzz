aaaba533b804cad569aee814ff1d9312d1f27629
gouravkhanna <gourav.khanna@osscube.com>
2013-04-12 03:33:21 -0700

    file upload

---
+++ views/midpanel/displayphotocomment.php
@@ -0,0 +1,32 @@
+<?PHP
+foreach ( @$arrData as $row ) {
+    $c = 1;
+    echo "<br>";
+    foreach ( $row as $value ) {
+    if (is_array ( $value )) {
+                $commentId=($value['id']);
+                echo '<div id=commentdel' . $commentId.'></div>';
+                echo '<br><div id="cmmnt' . $commentId . '"class="comments"><span class="x">';
+                echo "<img class='round5 imgcenterm' src='" .ROOTPATH. $value ['path'] . "' height='30px' width='30px'/>";
+                echo "<a href='" . ROOTPATH . "index.php?controller=friends&url=friendsProfile&friendId=" . $value ['user_id'] . "' >";
+                echo $value ['first_name'] . " " . $value ['last_name'];
+                echo "</a>";
+                echo "<span class='spancomment'> ";
+                echo $value ['comment_text'];
+                $timeAgo=time_elapsed ($value['comment_time']);
+                if($timeAgo=="") {
+                    $timeAgo="Just Now";
+                }
+                echo "</span><span class=floatr title='".date('d-M-Y',$value['comment_time'])."'>".$timeAgo."</span>";
+                if( $value['user_id']==@$_SESSION['id']) {
+                   
+                    echo "<span class='floatr marginr10 cursor1' onclick=commentDelete('$commentId')>X   </span>";
+                }
+                echo "</div>";
+                $c++;
+            }
+    }
+    echo "</div>";
+}
+
+?>
---
+++ views/photo/uploadPhoto.php
@@ -0,0 +1,42 @@
+
+<div id="bigmid">Upload New Photo
+<form id="form3" action="" method="POST"
+		enctype="multipart/form-data">
+		<div id="pLoad"></div>
+		<div id="a">
+			<input type="file" name="resume1[]" id="file" multiple="multiple"> 
+		</div>
+		<input type="submit" onsubmit="return false;">
+    </form>
+</div>
+
+<script type="text/javascript">
+var i=1;
+function addUpload()
+{
+	i=i+1;
+	if(i<25)
+	$("#a").append("<input type='file' name='resume"+i+"[]'  multiple='multiple' >New Name <input type='type' name='file"+i+"' > <br/>");
+}
+
+$("form#form3").submit(function(){
+	var formData = new FormData($(this)[0]);
+	formData.append("size",i);	
+    $.ajax({
+        url:'index.php?controller=photo&url=upload', //window.location.pathname,
+        type: 'POST',
+        data: formData,//+"&url="+10,
+        beforeSend:function(data){
+    		$("#pLoad").html("<img src='http://localhost/probuzz/trunk/data/photo/load3.gif' alt='loading' >");		    
+		},
+         success: function (data) {
+            $("#pLoad").html(data);
+        },
+        cache: false,
+        contentType: false,
+       processData: false
+    });
+
+    return false;
+});</script>
+</script>
